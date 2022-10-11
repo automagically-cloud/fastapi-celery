34ea2a38b8d5fc5f00af9937e214fc3e0a80c683
Jens Neuhaus <kontakt@jensneuhaus.de>
2022-10-11 21:18:29 +0200

    Update worker.py

--- project/worker.py
+++ project/worker.py
@@ -13,3 +13,9 @@ celery.conf.result_backend = os.environ.get("CELERY_RESULT_BACKEND", "redis://lo
 def create_task(task_type):
     time.sleep(int(task_type) * 10)
     return True
+
+
+@celery.task(name="error_task")
+def error_task(task_type):
+    1/0
+    return True
