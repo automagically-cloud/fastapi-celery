f0d50d7b1b40dedea7bef9ddd676481c9084fbf9
Jens Neuhaus <kontakt@jensneuhaus.de>
2022-10-13 17:47:03 +0200

    fix: INFO: unbound variable

--- start_celeryworker.sh
+++ start_celeryworker.sh
@@ -6,7 +6,7 @@ set -o nounset
 
 celery -A worker.celery worker \
     -E \
-    --loglevel=${CELERY_WORKER_LOGLEVEL:INFO} \
+    --loglevel $CELERY_WORKER_LOGLEVEL \
     --hostname $CELERY_WORKER_NAME@%h \
-    --concurrency=$CELERY_WORKER_CONCURRENCY \
+    --concurrency $CELERY_WORKER_CONCURRENCY \
     --queues $CELERY_WORKER_QUEUES
